d422e0f4168aea507d17c6fea55bd0ca8aa3d3e6
ashx3s <ashx3s@tuta.io>
2025-05-30 17:01:39 -0600

    basic cd command
    works with ls and other commands

---
+++ cdCmd.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const fs = require("node:fs/promises");
+const path = require("node:path");
+const { chdir, cwd }  = require("node:process");
+
+async function cdCmd(inputArg) {
+	// validate path
+	if (!inputArg) {
+		return 0;
+	}
+	// get current path
+	const newPath = path.format({
+		dir: cwd(), 
+		base: inputArg[0]
+	})
+	try {
+		await chdir(newPath);
+		console.log(`Current Directory: ${cwd()}`);
+	} 
+	catch (err) {
+		console.error(`chdir: ${err}`);
+	}
+}
+
+module.exports = cdCmd;
